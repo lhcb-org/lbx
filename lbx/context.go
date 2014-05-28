bea518414879286d458cb407644c10f9690848f0
Sebastien Binet <binet@cern.ch>
2014-05-28 19:10:52 +0200

    context: introduce projects-search-path

--- lbx/context.go
+++ lbx/context.go
@@ -1,16 +1,22 @@
 package lbx
 
 import (
+	"os"
+	"strings"
+
 	"github.com/gonuts/logger"
 )
 
 type Context struct {
-	msg *logger.Logger
+	msg          *logger.Logger
+	ProjectsPath []string // default (project) search path
 }
 
 func NewContext(name string) *Context {
+
 	return &Context{
-		msg: logger.New(name),
+		msg:          logger.New(name),
+		ProjectsPath: defaultProjectsPath(),
 	}
 }
 
@@ -36,4 +42,21 @@ func (ctx *Context) Debugf(format string, args ...interface{}) (int, error) {
 
 func (ctx *Context) Verbosef(format string, args ...interface{}) (int, error) {
 	return ctx.msg.Verbosef(format, args...)
+}
+
+// defaultProjectsPath returns the default search path for projects
+func defaultProjectsPath() []string {
+	projpath := make([]string, 0)
+	for _, k := range []string{"CMAKE_PREFIX_PATH", "CMTPROJECTPATH", "LHCBPROJECTPATH"} {
+		v := os.Getenv(k)
+		if v == "" {
+			continue
+		}
+		vv := strings.Split(v, string(os.PathListSeparator))
+		for _, p := range vv {
+			projpath = append(projpath, p)
+		}
+	}
+
+	return projpath
 }
