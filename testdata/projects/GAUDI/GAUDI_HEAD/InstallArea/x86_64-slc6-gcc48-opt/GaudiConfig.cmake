23f77e5a6874077520a45bdacd5a57ef796e691d
Sebastien Binet <binet@cern.ch>
2014-06-03 16:33:09 +0200

    test: add GaudiConfig.cmake to testdata

---
+++ testdata/projects/GAUDI/GAUDI_HEAD/InstallArea/x86_64-slc6-gcc48-opt/GaudiConfig.cmake
@@ -0,0 +1,15 @@
+# File automatically generated: DO NOT EDIT.
+set(Gaudi_heptools_version 68)
+set(Gaudi_heptools_system x86_64-slc6-gcc48)
+
+set(Gaudi_PLATFORM x86_64-slc6-gcc48-opt)
+
+set(Gaudi_VERSION HEAD)
+set(Gaudi_VERSION_MAJOR 999)
+set(Gaudi_VERSION_MINOR 999)
+set(Gaudi_VERSION_PATCH 0)
+
+set(Gaudi_USES )
+
+list(INSERT CMAKE_MODULE_PATH 0 ${Gaudi_DIR}/cmake)
+include(GaudiPlatformConfig)
