62ac9ca87997c8c89ced08046489154624afc291
Sebastien Binet <binet@cern.ch>
2014-06-03 15:40:15 +0200

    test: cleanup tests hierarchy

--- main_test.go
+++ main_test.go
@@ -12,13 +12,15 @@ func TestInit(t *testing.T) {
 		t.Fatalf("error setting LHCBPROJECTPATH: %v\n", err)
 	}
 
-	_ = os.RemoveAll("GaudiDev_trunk")
+	_ = os.RemoveAll("testdata/test-init")
 
-	cmd := exec.Command("lbx", "init", "-lvl=-2", "gaudi")
+	cmd := exec.Command("lbx", "init", "-lvl=-2", "-user-area=testdata/test-init", "gaudi")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err = cmd.Run()
 	if err != nil {
 		t.Fatalf("error running lbx-init: %v\n", err)
 	}
+
+	_ = os.RemoveAll("testdata/test-init")
 }
