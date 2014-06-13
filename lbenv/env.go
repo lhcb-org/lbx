72f780b1b691246007efadb0c2c8696d7c3b5f48
Sebastien Binet <binet@cern.ch>
2014-06-13 12:39:35 +0200

    lbenv: refactor

--- lbenv/env.go
+++ lbenv/env.go
@@ -3,7 +3,6 @@ package lbenv
 import (
 	"fmt"
 	"os"
-	"strings"
 )
 
 // Environment models the recipe(s) to craft and obtain a given environment
@@ -183,45 +182,4 @@ func (env *Environment) Keys() []string {
 		keys = append(keys, k)
 	}
 	return keys
-}
-
-// Var is an environment variable.
-type Var struct {
-	Name  string
-	Value string
-	Type  VarType
-	Local bool
-}
-
-func (v *Var) append(value string) {
-	switch v.Type {
-	case VarList:
-		v.Value += string(os.PathListSeparator) + value
-	case VarScalar:
-		v.Value += value
-	}
-}
-
-func (v *Var) prepend(value string) {
-	switch v.Type {
-	case VarList:
-		v.Value = value + string(os.PathListSeparator) + v.Value
-	case VarScalar:
-		v.Value = value + v.Value
-	}
-}
-
-func (v *Var) remove(value string) {
-	switch v.Type {
-	case VarList:
-		vals := make([]string, 0)
-		for _, vv := range splitpath(v.Value) {
-			if vv != value {
-				vals = append(vals, vv)
-			}
-		}
-		v.Value = strings.Join(vals, string(os.PathListSeparator))
-	case VarScalar:
-		v.Value = strings.Replace(v.Value, value, "", -1)
-	}
 }
