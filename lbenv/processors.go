0f38bc2889e7bf377d481731772c2dcfef6a3585
Sebastien Binet <binet@cern.ch>
2014-06-16 09:39:32 +0200

    lbenv: implement EmptyDirsRemover+UsePythonZip

--- lbenv/processors.go
+++ lbenv/processors.go
@@ -1,6 +1,8 @@
 package lbenv
 
 import (
+	"archive/zip"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -83,6 +85,52 @@ func DuplicatesRemover(v *Var, value string, env *Environment) string {
 		}
 		set[p] = struct{}{}
 		dirs = append(dirs, p)
+	}
+	return strings.Join(dirs, string(os.PathListSeparator))
+}
+
+// EmptyDirsRemover removes empty or non-existing directories from lists
+func EmptyDirsRemover(v *Var, value string, env *Environment) string {
+	if v.Type == VarScalar {
+		return value
+	}
+	paths := splitpath(value)
+	dirs := make([]string, 0, len(paths))
+	for _, dir := range paths {
+		fi, err := os.Stat(dir)
+		if err != nil {
+			continue
+		}
+		if !(strings.HasSuffix(dir, ".zip") || fi.IsDir()) {
+			continue
+		}
+		files, err := ioutil.ReadDir(dir)
+		if err != nil {
+			continue
+		}
+		if len(files) <= 0 {
+			continue
+		}
+		dirs = append(dirs, dir)
+	}
+	return strings.Join(dirs, string(os.PathListSeparator))
+}
+
+// UsePythonZip uses .zip files instead of regular directories in PYTHONPATH when possible.
+func UsePythonZip(v *Var, value string, env *Environment) string {
+	if v.Type == VarScalar || v.Name != "PYTHONPATH" {
+		return value
+	}
+	paths := splitpath(value)
+	dirs := make([]string, 0, len(paths))
+	for _, dir := range paths {
+		fname := dir + ".zip"
+		if f, err := zip.OpenReader(fname); err == nil {
+			f.Close()
+			dirs = append(dirs, fname)
+		} else {
+			dirs = append(dirs, dir)
+		}
 	}
 	return strings.Join(dirs, string(os.PathListSeparator))
 }
